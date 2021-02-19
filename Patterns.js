a37c3bd5f84d855bfe0204d585274d4fc85a5c44
jonnyhak <jonnyhak@gmail.com>
2021-02-18 20:07:27 -0500

    pointer

--- Patterns.js
+++ Patterns.js
@@ -20,10 +20,29 @@ function validAnagram(arr1, arr2){
 
 }
 
+// console.log(validAnagram('', ''))//true
+// console.log(validAnagram('aaz', 'zza'))//false
+// console.log(validAnagram('anagram', 'nagaram'))//true
+// console.log(validAnagram('anagram', 'anagramanagram'))//false
+// console.log(validAnagram('rat', 'car'))//false
 
-console.log(validAnagram('', ''))//true
-console.log(validAnagram('aaz', 'zza'))//false
-console.log(validAnagram('anagram', 'nagaram'))//true
-console.log(validAnagram('anagram', 'anagramanagram'))//false
-console.log(validAnagram('rat', 'car'))//false
+function countUniqueValues(arr){
+  // add whatever parameters you deem necessary - good luck!
+  i = 0
+  if (arr.length === 0){return 0}
+  for (let j = 1; j < arr.length; j++) {
+      if (arr[i] !== arr[j]){
+        i++  
+        arr[i] = arr[j]
+      }  
+  }
+  return i + 1
+}
+
+console.log(countUniqueValues([1, 1, 1, 1, 2])) // 2
+console.log(countUniqueValues([1,2,3,4,4,4,7,7,12,12,13])) //7
+console.log(countUniqueValues([-2, -1, -1, 0, 1])) //4
+console.log(countUniqueValues([])) //0
+
+
 
