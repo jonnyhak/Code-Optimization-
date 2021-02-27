07425bf45bf3b9edfde609a8661c5dd4cb25d4a7
jonnyhak <jonnyhak@gmail.com>
2021-02-26 19:25:20 -0500

    instance methods

--- SLL.js
+++ SLL.js
@@ -4,11 +4,23 @@ class Student {
         this.firstName = firstName;
         this.lastName = lastName;
         this.grade = year;
+        this.tardies = 0;
+    }
+    fullName(){
+        return `Your full name is ${this.firstName} ${this.lastName}`
+    }
+    markLate(){
+        this.tardies += 1
+        return `${this.firstName} ${this.lastName} has been late ${this.tardies} times`
     }
 }
 
 let firstStudent = new Student("Jonny", "Hak", 9);
 let secondStudent = new Student("Alex", "Hak", 8);
 
-console.log(firstStudent)
-console.log(secondStudent)
+secondStudent.grade = 2
+
+console.log(firstStudent.markLate())
+console.log(firstStudent.markLate())
+console.log(firstStudent.tardies)
+console.log(secondStudent.fullName())
