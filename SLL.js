48d96326666e492190997520517f730c44c15047
jonnyhak <jonnyhak@gmail.com>
2021-03-03 21:05:57 -0500

    sll

--- SLL.js
+++ SLL.js
@@ -23,8 +23,42 @@ let secondStudent = new Student("Alex", "Hak", 8);
 
 secondStudent.grade = 2
 
-console.log(firstStudent.markLate())
-console.log(firstStudent.markLate())
-console.log(firstStudent.tardies)
-console.log(secondStudent.fullName())
-console.log(Student.EnrollStudents())
+// console.log(firstStudent.markLate())
+// console.log(firstStudent.markLate())
+// console.log(firstStudent.tardies)
+// console.log(secondStudent.fullName())
+// console.log(Student.EnrollStudents())
+
+class Node{
+    constructor(val){
+        this.val = val;
+        this.next = null;
+    }
+}
+
+class SinglyLinkedList{
+    constructor(){
+        this.head = null;
+        this.tail = null;
+        this.length = 0
+    }
+    push(val){
+        let newNode = new Node(val);
+        if(!this.head){
+            this.head = newNode
+            this.tail = this.head
+        } else {
+            this.tail.next = newNode;
+            this.tail = newNode;
+        }
+        this.length++;
+        return this
+    }
+}
+
+let list = new SinglyLinkedList()
+list.push("HI")
+list.push("YOU")
+
+console.log(list)
+
