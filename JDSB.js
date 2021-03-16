1fc707939491670cdec7a7bf7a801aeeedbe4c61
jonnyhak <jonnyhak@gmail.com>
2021-03-16 16:48:02 -0400

    q

---
+++ JDSB.js
@@ -0,0 +1,24 @@
+
+
+//Sort the characters of a string
+
+//"xirb" => "birx"
+//"e" => "e"
+//"" => ""
+
+//
+//make an array of all letters
+//iterate over input string
+//check to see if following character has a lower index in alphabet
+//swap using ES6 swap
+//exit while loop once sorting is complete
+
+ "x i r b"
+//0 1 2 3
+ "i x r b"
+ "i r x b" //decrement index by 1
+ "i r b x"
+
+
+
+
---
+++ StacksQueues.js
@@ -0,0 +1,9 @@
+
+let q = [];
+
+q.push("First")
+q.push("Second")
+q.push("Third")
+
+console.log(q.shift())
+console.log(q)
