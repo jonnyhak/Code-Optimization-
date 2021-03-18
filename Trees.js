c9cf8b82a6b6452be41f5c727ecc939348d60291
jonnyhak <jonnyhak@gmail.com>
2021-03-18 17:29:45 -0400

    trees

--- Trees.js
+++ Trees.js
@@ -14,10 +14,27 @@
      insert(value){
          let newNode = new Node(value);
          if(this.root === null){
-             this.root = newNode;
-             return this;
+            this.root = newNode;
+            return this;
          } else {
-             
+            let current = this.root;
+            while(true){
+                if(value < current.value){
+                    if(current.left === null){
+                        current.left = newNode
+                        return this;
+                    } else {
+                        current = current.left;
+                    }
+                } else if (value > current.value){
+                    if (current.right === null){
+                        current.right = newNode;
+                        return this;
+                    } else {
+                        current = current.right;
+                    }
+                }
+            }
          }
      }
 
@@ -25,5 +42,11 @@
 
  let tree = new BinarySearchTree();
  tree.insert(10)
+ tree.insert(5)
+ tree.insert(2)
+ tree.insert(13)
+ tree.insert(11)
+ tree.insert(16)
+ 
 
  console.log(tree)
