088a77ef3c5e7aa3404226b1a0f1381dbf52dc87
jonnyhak <jonnyhak@gmail.com>
2021-03-27 11:59:05 -0400

    construct

--- BH.js
+++ BH.js
@@ -1,2 +1,14 @@
 
-console.log("Binary Heaps")
+
+class MaxBinaryHeap {
+    constructor(){
+        this.values =[]
+    }
+    insert(element){
+        this.value.push(element);
+        this.bubbleUp();
+    }
+    bubbleUp(){
+        
+    }
+}
