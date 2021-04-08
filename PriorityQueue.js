1639d8a1393848fcf0088ba92d6047e72149118a
jonnyhak <jonnyhak@gmail.com>
2021-04-08 16:03:40 -0400

    dequeue

--- PriorityQueue.js
+++ PriorityQueue.js
@@ -1,4 +1,3 @@
-
 class PriorityQueue {
     constructor(){
         this.values =[]
@@ -14,20 +13,20 @@ class PriorityQueue {
         while(idx > 0){
             let parentIdx = Math.floor((idx - 1)/2)
             let parent = this.values[parentIdx];
-            if(element.priority <= parent.priority) break;
+            if(element.priority >= parent.priority) break;
             this.values[parentIdx] = element;
             this.values[idx] = parent
             idx = parentIdx
         }
     }
     dequeue(){
-        let max = this.values[0]
+        let min = this.values[0]
         let end = this.values.pop()
         if(this.values.length > 0){
             this.values[0] = end;
             this.sinkDown();
         }
-        return max
+        return min
     }
     sinkDown(){
         let idx = 0;
@@ -41,7 +40,7 @@ class PriorityQueue {
 
             if (leftChildIdx < length){
                 leftChild = this.values[leftChildIdx];
-                if(leftChild.priority > element.priority){
+                if(leftChild.priority < element.priority){
                     swap = leftChildIdx
                 }
             }
@@ -68,8 +67,14 @@ class Node {
 }
 
 let ER = new PriorityQueue();
-ER.enqueue("common cold", 1)
-ER.enqueue("gunshot wound", 5)
-ER.enqueue("high fever", 2)
+ER.enqueue("common cold", 5)
+ER.enqueue("gunshot wound", 1)
+ER.enqueue("high fever", 4)
+ER.enqueue("glass in foot", 3)
+ER.enqueue("broken arm", 2)
 
 console.log(ER)
+console.log(ER.dequeue())
+console.log(ER.dequeue())
+console.log(ER.dequeue())
+console.log(ER.dequeue())
